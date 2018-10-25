7bdb302eece4f2180386fbe37de19cb4273031c5
emmanuelmacharia <samuelmarsha@outlook.com>
2018-10-25 11:58:22 +0300

    [#161411924] set up the database connection with postgre and psycopg2(init.py)

--- app/api/v2/models/__init__.py
+++ app/api/v2/models/__init__.py
@@ -1,10 +1,8 @@
 
 import psycopg2
 DATABASE_URL ="dbname='storemanager' host='localhost' port=5432 user='postgres' password='Permafrost'"
-conn = psycopg2.connect(DATABASE_URL)
 
-if conn == True:
-    print(conn)
+conn = psycopg2.connect(DATABASE_URL)
 
 cur = conn.cursor()
 
--- instance/config.py
+++ instance/config.py
@@ -4,6 +4,15 @@ class Configurations():
     DEBUG = False
     TESTING = False
 
+# parameters = {
+#     "dbname":os.getenv("storemanager"),
+#     "host":os.getenv("localhost"),
+#     "port":os.getenv(5432),
+#     "user":os.getenv("postgres"),
+#     "password":os.getenv("Permafrost")
+# }
+#{}"dbname='storemanager' host='localhost' port=5432 user='postgres' password='Permafrost'"
+
 class Development(Configurations):
     DEBUG = True
 
