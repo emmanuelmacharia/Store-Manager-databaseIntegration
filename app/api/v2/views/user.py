0d43b632a3ccda24c9e0d37011ae989760c1bf5f
emmanuelmacharia <samuelmarsha@outlook.com>
2018-10-26 03:45:02 +0300

    [#161495182] created endpoints for creating, getting and deleting a user

--- app/api/v2/views/user.py
+++ app/api/v2/views/user.py
@@ -4,7 +4,8 @@ from flask import Flask, request, jsonify, make_response
 from flask_restful import Api, Resource
 from flask_jwt_extended import  create_access_token, jwt_required, create_refresh_token, jwt_refresh_token_required, get_jwt_identity;
 
-from models import Products
+from models import User
+from views.utils import user_valid
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,8 +14,31 @@ api = Api(app)
 class User(Resource):
     '''defines the get-all,post, and delete user methods'''
     def get(self):
-        pass
+        return User.viewall(),200
+
     def post(self):
-        pass
+        data = request.get_json()
+        username = data['username']
+        email = data['email']
+        password = data['password']
+
+        result = user_valid(username,email,password)
+        if result == True:
+            hash = User.generate_hash(password)
+        else:
+            return 'please enter data in the right format', 400
+
+        try:
+            new_user = User.save(username, email, hash, role)
+            ac_token = create_access_token(identity = data['email'])
+            new_token = create_refresh_token(identity = data['email'])
+            return {'message': 'new user created','access_token':ac_token,'refresh_token':new_token}, 201
+        except:
+            return {'message':'Hmmm...something here\'s afoot'}, 404
+
     def delete(self):
-        pass
+        id = parser.add_argument('id', type=int, help='id must be an integer')
+        args = parser.parse_args()
+
+        result = Products()
+        result.delete(args)
